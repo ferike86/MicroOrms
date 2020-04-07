2e551bffdba50d94687d72385d4d2e45e1ffe248
ferike86 <tundikferenc@freemail.hu>
2020-04-07 18:55:07 +0200

    Fix id generation in user table.

--- CreateUserTable.sql
+++ CreateUserTable.sql
@@ -5,7 +5,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[user](
-	[id] [bigint] NOT NULL,
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
 	[name] [nvarchar](50) NULL,
  CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED 
 (
